bb3ea08ec9213f021da90837142ce371ffb5dfda
付翾 <fuxuan@sinosoft.com.cn>
2025-03-22 17:36:14 +0800

    客户信息调整

--- db/table/table-user.sql
+++ db/table/table-user.sql
@@ -1,7 +1,7 @@
 -- 若需删除 user 表，可取消此注释
 -- DROP TABLE IF EXISTS user;
 -- 用户表，存储所有类型用户的通用信息
-CREATE TABLE user
+CREATE TABLE `user`
 (
     id                   BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '用户唯一标识，自增主键',
     username             VARCHAR(50)  NOT NULL UNIQUE COMMENT '用户名，唯一且不能为空',
@@ -11,19 +11,19 @@ CREATE TABLE user
     phone_number         VARCHAR(20) UNIQUE COMMENT '用户电话号码，唯一',
     user_type            VARCHAR(50)  NOT NULL COMMENT '用户类型，取值可为：corporate（法人客户）、individual（个人客户）、lawyer（律师）、admin（管理员）',
     source_type          VARCHAR(50) DEFAULT 'manual_register' COMMENT '数据来源类型，取值可为：database_insert（数据库插入）、manual_register（人工系统注册），默认是人工系统注册',
-    related_entity_id    BIGINT COMMENT '关联的实体 ID，根据 user_type 确定关联的表：corporate 对应 corporate_clients 表，individual 对应 individual_clients 表，lawyer 对应 lawyers 表，admin 对应 administrators 表',
+    related_entity_id    BIGINT COMMENT '关联的实体 ID，根据 user_type 确定关联的表：corporate 对应 corporate_client 表，individual 对应 individual_client 表，lawyer 对应 lawyers 表，admin 对应 administrator 表',
     is_valid_flag        CHAR(1)     DEFAULT '1' COMMENT '用户是否有效，1 表示有效，0 表示无效，默认为有效',
     insert_time_for_his  TIMESTAMP   DEFAULT CURRENT_TIMESTAMP COMMENT '用户信息插入时间，默认为当前时间',
     operate_time_for_his TIMESTAMP   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '用户信息操作时间，自动更新'
 );
-CREATE INDEX idx_user_related_entity_id ON user (related_entity_id);
-CREATE INDEX idx_user_insert_time_for_his ON user (insert_time_for_his);
-CREATE INDEX idx_user_operate_time_for_his ON user (operate_time_for_his);
+CREATE INDEX idx_user_related_entity_id ON `user` (related_entity_id);
+CREATE INDEX idx_user_insert_time_for_his ON `user` (insert_time_for_his);
+CREATE INDEX idx_user_operate_time_for_his ON `user` (operate_time_for_his);
 
--- 若需删除 corporate_clients 表，可取消此注释
--- DROP TABLE IF EXISTS corporate_clients;
+-- 若需删除 corporate_client 表，可取消此注释
+-- DROP TABLE IF EXISTS corporate_client;
 -- 法人客户表，存储法人客户的额外信息
-CREATE TABLE corporate_clients
+CREATE TABLE corporate_client
 (
     id                   BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '法人客户唯一标识，自增主键',
     company_name         VARCHAR(200) NOT NULL COMMENT '公司名称，不能为空',
@@ -34,13 +34,13 @@ CREATE TABLE corporate_clients
     insert_time_for_his  TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '法人客户信息插入时间，默认为当前时间',
     operate_time_for_his TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '法人客户信息操作时间，自动更新'
 );
-CREATE INDEX idx_corporate_clients_insert_time_for_his ON corporate_clients (insert_time_for_his);
-CREATE INDEX idx_corporate_clients_operate_time_for_his ON corporate_clients (operate_time_for_his);
+CREATE INDEX idx_corporate_client_insert_time_for_his ON corporate_client (insert_time_for_his);
+CREATE INDEX idx_corporate_client_operate_time_for_his ON corporate_client (operate_time_for_his);
 
--- 若需删除 individual_clients 表，可取消此注释
--- DROP TABLE IF EXISTS individual_clients;
+-- 若需删除 individual_client 表，可取消此注释
+-- DROP TABLE IF EXISTS individual_client;
 -- 个人客户表，存储个人客户的额外信息
-CREATE TABLE individual_clients
+CREATE TABLE individual_client
 (
     id                   BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '个人客户唯一标识，自增主键',
     full_name            VARCHAR(100) NOT NULL COMMENT '个人客户全名，不能为空',
@@ -50,13 +50,13 @@ CREATE TABLE individual_clients
     insert_time_for_his  TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '个人客户信息插入时间，默认为当前时间',
     operate_time_for_his TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '个人客户信息操作时间，自动更新'
 );
-CREATE INDEX idx_individual_clients_insert_time_for_his ON individual_clients (insert_time_for_his);
-CREATE INDEX idx_individual_clients_operate_time_for_his ON individual_clients (operate_time_for_his);
+CREATE INDEX idx_individual_client_insert_time_for_his ON individual_client (insert_time_for_his);
+CREATE INDEX idx_individual_client_operate_time_for_his ON individual_client (operate_time_for_his);
 
--- 若需删除 administrators 表，可取消此注释
--- DROP TABLE IF EXISTS administrators;
+-- 若需删除 administrator 表，可取消此注释
+-- DROP TABLE IF EXISTS administrator;
 -- 管理员表，存储管理员的额外信息
-CREATE TABLE administrators
+CREATE TABLE administrator
 (
     id                   BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '管理员唯一标识，自增主键',
     admin_role           VARCHAR(50) COMMENT '管理员角色',
@@ -66,7 +66,7 @@ CREATE TABLE administrators
     insert_time_for_his  TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '管理员信息插入时间，默认为当前时间',
     operate_time_for_his TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '管理员信息操作时间，自动更新'
 );
-CREATE INDEX idx_administrators_user_id ON administrators (id);
-CREATE INDEX idx_administrators_parent_admin_id ON administrators (parent_admin_id);
-CREATE INDEX idx_administrators_insert_time_for_his ON administrators (insert_time_for_his);
-CREATE INDEX idx_administrators_operate_time_for_his ON administrators (operate_time_for_his);
+CREATE INDEX idx_administrator_id ON administrator (id);
+CREATE INDEX idx_administrator_parent_admin_id ON administrator (parent_admin_id);
+CREATE INDEX idx_administrator_insert_time_for_his ON administrator (insert_time_for_his);
+CREATE INDEX idx_administrator_operate_time_for_his ON administrator (operate_time_for_his);
