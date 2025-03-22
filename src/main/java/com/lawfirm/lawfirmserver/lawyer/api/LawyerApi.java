912c9f71611d4f7ce66fa44732e2f21b94e3097f
付翾 <fuxuan@sinosoft.com.cn>
2025-03-22 18:27:36 +0800

    律师信息调整

--- src/main/java/com/lawfirm/lawfirmserver/lawyer/api/LawyerApi.java
+++ src/main/java/com/lawfirm/lawfirmserver/lawyer/api/LawyerApi.java
@@ -45,4 +45,32 @@ public class LawyerApi {
         lawyerVo = lawyerService.getLawyer(lawyerVo.getId());
         return lawyerVo;
     }
+
+    /**
+     * 处理获取律师信息的请求，并返回相应的律师信息视图对象。
+     * <p>
+     * 该方法通过 @RequestMapping 注解映射到 "/getLawyerInfo" 路径，
+     * 并且只处理 HTTP 的 POST 请求。
+     * 它接受一个包含律师信息的 LawyerVo 对象作为请求体（通过 @RequestBody 注解），
+     * 同时接受一个 HttpServletRequest 对象，用于获取请求相关的信息（虽然目前代码中未使用）。
+     * <p>
+     * 在方法内部，调用 lawyerService 的 getLawyer 方法，
+     * 根据传入的 lawyerVo 对象中的律师 ID，从数据库中查询出对应的律师信息，
+     * 并将查询结果重新赋值给 lawyerVo 变量。
+     * <p>
+     * 最后，将包含律师信息的 lawyerVo 对象返回给客户端，
+     * 以便客户端获取该律师的详细信息。
+     *
+     * @param lawyerVo 包含律师 ID 等信息的视图对象，通过请求体传入，用于指定要获取信息的律师
+     * @param request  HttpServletRequest 对象，可用于获取请求相关的信息，如请求头、请求参数等
+     * @return 包含律师详细信息的 LawyerVo 对象，从数据库中查询得到并返回给客户端
+     */
+    @RequestMapping(value = "/getLawyerInfo", method = {RequestMethod.POST})
+    public LawyerVo getLawyerInfo(@RequestBody LawyerVo lawyerVo, HttpServletRequest request) {
+        // 调用 lawyerService 的 getLawyer 方法，根据 lawyerVo 中的律师 ID 从数据库中获取律师信息
+        lawyerVo = lawyerService.getLawyer(lawyerVo.getId());
+        // 将获取到律师信息的 lawyerVo 对象返回给客户端
+        return lawyerVo;
+    }
+
 }
