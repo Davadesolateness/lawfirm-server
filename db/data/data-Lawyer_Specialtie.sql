d63ce6c53fbfe19b03dd97af54d56ff8a1c9b47b
付翾 <fuxuan@sinosoft.com.cn>
2025-03-27 20:58:36 +0800

    律师专长基础数据

---
+++ db/data/data-Lawyer_Specialtie.sql
@@ -0,0 +1,16 @@
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('刑事案件');
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('经济纠纷');
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('劳动纠纷');
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('工伤纠纷');
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('交通事故');
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('房产纠纷');
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('土地纠纷');
+INSERT INTO Lawyer_Specialtie (specialty_Name)
+VALUES ('离婚纠纷');
