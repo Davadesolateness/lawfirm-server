8b6a6dd68db766e0bce8e04d2405211d66f34add
付翾 <fuxuan@sinosoft.com.cn>
2025-04-01 20:51:34 +0800

    律师信息调整

---
+++ db/data/data-LawyerSpecialtie.sql
@@ -0,0 +1,17 @@
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('刑事案件');
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('经济纠纷');
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('劳动纠纷');
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('工伤纠纷');
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('交通事故');
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('房产纠纷');
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('土地纠纷');
+INSERT INTO lawyerspecialty (specialtyName)
+VALUES ('离婚纠纷');
+
