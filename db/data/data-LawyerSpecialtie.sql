101d2c777a6df930af57f6e22a518bf19bcda665
付翾 <fuxuan@sinosoft.com.cn>
2025-04-09 21:08:36 +0800

    大小写问题调整

--- db/data/data-LawyerSpecialtie.sql
+++ db/data/data-LawyerSpecialtie.sql
@@ -1,17 +1,16 @@
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('刑事案件');
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('经济纠纷');
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('劳动纠纷');
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('工伤纠纷');
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('交通事故');
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('房产纠纷');
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('土地纠纷');
-INSERT INTO lawyerspecialty (specialtyName)
+INSERT INTO lawyerSpecialty (specialtyName)
 VALUES ('离婚纠纷');
-
